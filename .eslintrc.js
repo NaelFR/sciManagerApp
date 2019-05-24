e22691018fe2c22a4aa66dc767c17a2dfa3152cd
Naël Abou chahine <abou.chahine.nael@gmail.com>
2019-05-24 11:08:50 +0200

    set indent spaces 4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,22 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ["plugin:vue/essential"],
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        "indent": ["error", 4],
+        "vue/html-indent": ["error", 4, {
+            "attribute": 1,
+            "baseIndent": 1,
+            "closeBracket": 0,
+            "alignAttributesVertically": true,
+            "ignores": [],
+        }]
+    },
+    parserOptions: {
+        parser: "babel-eslint"
+    }
 };
